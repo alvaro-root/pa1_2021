064ed8bad67b56a749bb1e99d1c3e8c25cfd3e92
Mark Foley <mark@markfoley.info>
2020-10-22 18:24:08 +0100

    Latest

--- guessing_game.py
+++ guessing_game.py
@@ -9,26 +9,29 @@ All non-typed variables are integers: Rule 4
 
 import random  # get the random number module
 UPPER_LIMIT = 100  # set an upper limit
-number = random.randint(0, UPPER_LIMIT)  # get a random number
-# between 0 and 100 inclusive
+number = random.randint(0, UPPER_LIMIT)  # get a random number between 0 and 100 inclusive
 print("Hi-Lo Number Guessing Game: between 0 and 100 inclusive.\n")
 
-# get an initial guess
-guess_str = input("Guess a number: ")
-guess = int(guess_str)  # convert string to number 
-
-# while guess is range, keep asking
-while 0 <= guess <= UPPER_LIMIT:
-    if guess > number:
-        print(f"Guessed Too High. {guess}")
-    elif guess < number:
-        print(f"Guessed Too Low. {guess}")
-    else:  # correct guess, exit with break
-        print(f"You guessed it. The number was: {number}")
-        break
-    # keep going, get the next guess
+try:
+    # get an initial guess
     guess_str = input("Guess a number: ")
-    guess = int(guess_str)
-else:
-    print(f"You quit early, the number was: {number}")
-print("Finished.")
+    guess = int(guess_str)  # convert string to number
+
+    # while guess is range, keep asking
+    while 0 <= guess <= UPPER_LIMIT:
+        if guess > number:
+            print(f"Guessed Too High. {guess}")
+        elif guess < number:
+            print(f"Guessed Too Low. {guess}")
+        else:  # correct guess, exit with break
+            print(f"You guessed it. The number was: {number}")
+            break
+        # keep going, get the next guess
+        guess_str = input("Guess a number: ")
+        guess = int(guess_str)
+    else:
+        print(f"You quit early, the number was: {number}")
+    print("Finished.")
+except ValueError as e:
+    print(f"{'='*50}\nSomething bad happened!\n{e}\n{'='*50}")
+    quit(1)
