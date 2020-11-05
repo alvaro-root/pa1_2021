56f84308aa1250ab21a62bec205ae950cb3651db
Mark Foley <mark@markfoley.info>
2020-11-05 18:22:16 +0000

    Latest

--- test_import.py
+++ test_import.py
@@ -4,4 +4,4 @@ my_url = "https://markfoley.info/pa1/gettysburg.txt"
 
 gettysburg_text = rfn.get_file_from_net(my_url)
 
-print(my_url)
+print(f"Contents of {my_url}\n{'='*50}\n{gettysburg_text}\n{'='*50}")
