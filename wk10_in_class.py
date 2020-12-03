568f7e9d3d4d81d4e938c7cbfb8cc5557f09c2f2
Mark Foley <mark@markfoley.info>
2020-12-03 18:40:52 +0000

    Latest

--- wk10_in_class.py
+++ wk10_in_class.py
@@ -25,12 +25,12 @@ def my_function(param, param2=27):
         pass
 
 
-def my_fun_with_arbitrary_list(*args, **kwargs):
-    for item in args:
-        print(f"item is {item}")
-
-    for k, v in kwargs.items():
-        print(f"{k}: {v}")
+# def my_fun_with_arbitrary_list(*args, **kwargs):
+#     for item in args:
+#         print(f"item is {item}")
+#
+#     for k, v in kwargs.items():
+#         print(f"{k}: {v}")
 
 
 my_function(arg, 99)
@@ -41,11 +41,11 @@ for i in range(5):
     my_function(arg_list)
     print(f"arg is {arg_list} in the global NS")
 
-for i in range(10):
-    if not i % 2:
-        continue
+# for i in range(10):
+#     if not i % 2:
+#         continue
+#
+#     print(f"{i} is odd")
 
-    print(f"{i} is odd")
-
-a_new_list = [1, 2, 3, 4, "a", "xyz"]
-my_fun_with_arbitrary_list(9, 8, 7, a=123, b=234, xyz="abc")
+# a_new_list = [1, 2, 3, 4, "a", "xyz"]
+# my_fun_with_arbitrary_list(9, 8, 7, a=123, b=234, xyz="abc")
